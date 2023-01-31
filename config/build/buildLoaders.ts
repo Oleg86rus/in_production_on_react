9b204f5753faaa0701e977d5667244049b56c215
oleg086rus <o.pototskiy@yandex.ru>
2023-01-31 16:27:32 +0500

    scss in webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,12 +1,27 @@
 import webpack from "webpack";
 
 export function buildLoaders(): webpack.RuleSetRule[] {
+
+    const sccLoader =  {
+            test: /\.s[ac]ss$/i,
+            use: [
+                // Creates `style` nodes from JS strings
+                "style-loader",
+                // Translates CSS into CommonJS
+                "css-loader",
+                // Compiles Sass to CSS
+                "sass-loader",
+            ],
+    }
+
+//  Если не используем TS - нужен babel-loader
     const typescriptLoader =  {
         test: /\.tsx?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
     }
     return [
-        typescriptLoader
+        typescriptLoader,
+        sccLoader
     ]
 }
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export function someFn (arg: number): string {
-	console.log('RANDOM FUNCTION  ывавыа выавы аывавыаываыв вы')
-
-	return ""
-}
