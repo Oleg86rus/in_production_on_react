7a873ba1a2214621e2a175879ff6ae01d376c328
oleg086rus <o.pototskiy@yandex.ru>
2023-02-17 18:46:03 +0500

    add stories for widgets and shared

---
+++ src/widgets/LangSwitcher/ui/LangSwitcher.stories.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import 'app/styles/index.scss';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { LangSwitcher } from './LangSwitcher';
+
+export default {
+    title: 'widgets/LangSwitcher',
+    component: LangSwitcher,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+    args: {
+        to: '/',
+    },
+} as ComponentMeta<typeof LangSwitcher>;
+
+const Template: ComponentStory<typeof LangSwitcher> = (args) => <LangSwitcher {...args} />;
+
+export const Light = Template.bind({});
+Light.args = {};
+
+export const Dark = Template.bind({});
+Dark.args = {};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
