d6a7589f835d212f5db01ca80e57da277ee4ea88
oleg086rus <o.pototskiy@yandex.ru>
2023-02-18 12:11:45 +0500

    fix linterror i18n in Navbar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,29 +1,33 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkThem } from 'shared/ui/AppLink/AppLink';
-import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string
 }
 
-export const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink
-                theme={AppLinkThem.SECONDARY}
-                to="/"
-                className={cls.mainLink}
-            >
-                Главная
-            </AppLink>
-            <AppLink
-                theme={AppLinkThem.SECONDARY}
-                to="/about"
-            >
-                О сайте
-            </AppLink>
-        </div>
+export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
 
-    </div>
-);
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink
+                    theme={AppLinkThem.SECONDARY}
+                    to="/"
+                    className={cls.mainLink}
+                >
+                    {t('Главная')}
+                </AppLink>
+                <AppLink
+                    theme={AppLinkThem.SECONDARY}
+                    to="/about"
+                >
+                    {t('О сайте')}
+                </AppLink>
+            </div>
+
+        </div>
+    );
+};
