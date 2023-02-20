6c9eb2bb36929f1232406438cca8a3e19c7f8fd2
oleg086rus <o.pototskiy@yandex.ru>
2023-02-21 01:44:56 +0500

    add redux, add first entity

---
+++ src/entities/Counter/ui/Counter.test.tsx
@@ -0,0 +1,29 @@
+import { fireEvent, screen } from '@testing-library/react';
+import { ComponentRender } from 'shared/config/tests/componentRender/ComponentRender';
+import { userEvent } from '@storybook/testing-library';
+import { Counter } from './Counter';
+
+describe('Counter', () => {
+    test('Test title', () => {
+        ComponentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        expect(screen.getByTestId('value-title')).toHaveTextContent('10');
+    });
+
+    test('increment', () => {
+        ComponentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        userEvent.click(screen.getByTestId('increment-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('11');
+    });
+
+    test('decrement', () => {
+        ComponentRender(<Counter />, {
+            initialState: { counter: { value: 10 } },
+        });
+        userEvent.click(screen.getByTestId('decrement-btn'));
+        expect(screen.getByTestId('value-title')).toHaveTextContent('9');
+    });
+});
--- src/shared/config/tests/componentRender/ComponentRender.tsx
+++ src/shared/config/tests/componentRender/ComponentRender.tsx
@@ -2,23 +2,28 @@ import { ReactNode } from 'react';
 import { I18nextProvider } from 'react-i18next';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { StateSchema, StoreProvider } from 'app/providers/StoreProvider';
+import { DeepPartial } from '@reduxjs/toolkit';
 import i18nForTests from '../../i18n/i18nForTests';
 
 export interface ComponentRenderOptions {
-    route?: string
+    route?: string,
+    initialState?: DeepPartial<StateSchema>
 }
 
 export function ComponentRender(component: ReactNode, options: ComponentRenderOptions = {}) {
     const {
         route = '/',
+        initialState,
     } = options;
 
     return render(
-        <MemoryRouter initialEntries={[route]}>
-            <I18nextProvider i18n={i18nForTests}>
-                {component}
-            </I18nextProvider>
-        </MemoryRouter>
-        ,
+        <StoreProvider initialState={initialState}>
+            <MemoryRouter initialEntries={[route]}>
+                <I18nextProvider i18n={i18nForTests}>
+                    {component}
+                </I18nextProvider>
+            </MemoryRouter>
+        </StoreProvider>,
     );
 }
