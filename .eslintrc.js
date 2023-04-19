f1c39c00536df554fd0017377c266cf60e770992
oleg086rus <o.pototskiy@yandex.ru>
2023-04-19 22:20:24 +0500

    create npm pack, fix style, next step

--- .eslintrc.js
+++ .eslintrc.js
@@ -24,6 +24,7 @@ module.exports = {
         '@typescript-eslint',
         'i18next',
         'react-hooks',
+        'prod',
     ],
     rules: {
         indent: [2, 4],
@@ -65,6 +66,7 @@ module.exports = {
         ],
         'no-undef': 'off',
         'react/no-array-index-key': 'off',
+        'prod/path-checker': 'error',
     },
     globals: {
         __IS_DEV__: true,
