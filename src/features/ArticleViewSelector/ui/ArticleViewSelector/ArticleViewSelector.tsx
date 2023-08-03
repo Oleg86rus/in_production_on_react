7431ab541de2f3ce68ba5e232fd3d1d542e7b866
oleg086rus <o.pototskiy@yandex.ru>
2023-08-03 22:56:53 +0500

    some fixes in imports

--- src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
+++ src/features/ArticleViewSelector/ui/ArticleViewSelector/ArticleViewSelector.tsx
@@ -4,7 +4,7 @@ import ListIcon from '@/shared/assets/icons/list-24-24.svg';
 import TiledIcon from '@/shared/assets/icons/tiled-24-24.svg';
 import { Button, ThemeButton } from '@/shared/ui/Button';
 import { Icon } from '@/shared/ui/Icon';
-import cls from './ArticleViewSelector.tsx.module.scss';
+import cls from './ArticleViewSelector.module.scss';
 import { ArticleView } from '@/entities/Article';
 
 interface ArticleTextBlockComponentProps {
